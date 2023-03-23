2f6165cf907f95c1bba9e970e93242d4b36e6b66
Yash <yvyashvarma@gmail.com>
2023-03-24 02:02:30 +0530

    All challenges completed Fundamentals 2

--- script.js
+++ script.js
@@ -103,6 +103,7 @@ var checkWinner = (averageDolphin, averageKoala) => {
 // actually be the returned value of a function! So you can just call a function as array
 // values (so don't store the tip values in separate variables first, but right in the new
 // array) 😉
+
 function tipCalculator(bill) {
   return (tip = bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2);
 }
@@ -121,4 +122,112 @@ function displayBill(bill, tip) {
   );
 }
 
-tipData();
+// tipData();
+
+// Coding Challenge #3
+// Let's go back to Mark and John comparing their BMIs! This time, let's use objects to
+// implement the calculations! Remember: BMI = mass / height ** 2 = mass
+// / (height * height) (mass in kg and height in meter)
+// Your tasks:
+// 1. For each of them, create an object with properties for their full name, mass, and
+// height (Mark Miller and John Smith)
+// 2. Create a 'calcBMI' method on each object to calculate the BMI (the same
+// method on both objects). Store the BMI value to a property, and also return it
+// from the method
+// 3. Log to the console who has the higher BMI, together with the full name and the
+// respective BMI. Example: "John's BMI (28.3) is higher than Mark's (23.9)!"
+// Test data: Marks weights 78 kg and is 1.69 m tall. John weights 92 kg and is 1.95 m
+// tall.
+// GOOD LUCK 😀
+
+const mark = {
+  weight: 78,
+  height: 1.69,
+  weight2: 95,
+  height2: 1.95,
+  calculateBMI: function () {
+    this.bmi = this.weight / this.height ** 2;
+    return this.bmi;
+  },
+};
+const john = {
+  weight: 92,
+  height: 1.95,
+  weight2: 85,
+  height2: 1.76,
+  calculateBMI: function () {
+    this.bmi = this.weight / this.height ** 2;
+    return this.bmi;
+  },
+};
+// mark.calculateBMI();
+// john.calculateBMI();
+// if (mark.bmi > john.bmi) {
+//   console.log(`BMI of mark ${mark.bmi}is greater than johns ${john.bmi}`);
+// } else {
+//   console.log(`BMI of john ${john.bmi}is greater than mark ${mark.bmi}`);
+// }
+
+// Coding Challenge #4
+// Let's improve Steven's tip calculator even more, this time using loops!
+// Your tasks:
+// 1. Create an array 'bills' containing all 10 test bill values
+// 2. Create empty arrays for the tips and the totals ('tips' and 'totals')
+// 3. Use the 'calcTip' function we wrote before (no need to repeat) to calculate
+// tips and total values (bill + tip) for every bill value in the bills array. Use a for
+// loop to perform the 10 calculations!
+// Test data: 22, 295, 176, 440, 37, 105, 10, 1100, 86 and 52
+// Hints: Call ‘calcTip ‘in the loop and use the push method to add values to the
+// tips and totals arrays 😉
+// Bonus:
+// 4. Bonus: Write a function 'calcAverage' which takes an array called 'arr' as
+// an argument. This function calculates the average of all numbers in the given
+// array. This is a difficult challenge (we haven't done this before)! Here is how to
+// solve it:
+// 4.1.
+// First, you will need to add up all values in the array. To do the addition,
+// start by creating a variable 'sum' that starts at 0. Then loop over the
+// array using a for loop. In each iteration, add the current value to the
+// 'sum' variable. This way, by the end of the loop, you have all values
+// added together
+// 4.2.
+// To calculate the average, divide the sum you calculated before by the
+// length of the array (because that's the number of elements)
+// 4.3.
+// Call the function with the 'totals' array
+// GOOD LUCK 😀
+
+function tipCalculator2(bill) {
+  return (tip = bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2);
+}
+
+function tipData2() {
+  var bills = [275, 40, 430];
+  var tips = [];
+  var total = [];
+  for (const bill of bills) {
+    var tip = tipCalculator2(bill);
+    tips.push(tip);
+    total.push(tip + bill);
+    displayBill2(bill, tip);
+  }
+  displayTotal(total);
+}
+
+function displayBill2(bill, tip) {
+  console.log(
+    `The bill was ${bill}, tip is ${tip}, the total is ${bill + tip}`
+  );
+}
+
+function displayTotal(total) {
+  var sum = 0;
+  for (iterator of total) {
+    sum = sum + iterator;
+  }
+  console.log(sum);
+  var average = sum / total.length;
+  console.log(average);
+}
+
+tipData2();
